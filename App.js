b6b238d05574f3431184245aa03da7dbeffa26dd
AtallahFatma <fatma.atallah@gmail.com>
2021-11-18 15:45:02 +0100

    use hook for screen Dimensions

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, Image, SafeAreaView, Button, Alert } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, Image, SafeAreaView, Button, Alert, Platform, View } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
   const handlePress = () => console.log('text pressed');
+  const { landscape } = useDeviceOrientation();
+  console.log(useDimensions())
   return (
+    // SafeAreaView doesn't work on android so we added padding top on container style
     <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: "100%",
+        height: landscape ? "100%" : "30%"
+      }}>
+
+      </View>
       <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app!!!!!!!!!!! jbfze zejfbze kjzbef kjbezfo jbzeofdb kzbefoz</Text>
       <StatusBar style="auto" />
       <Image source={require("./assets/favicon.png")} />
@@ -29,7 +40,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fee',
-    justifyContent: 'center',
-    alignItems: 'center'
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   }
 });
