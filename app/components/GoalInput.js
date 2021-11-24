37a962679fa5549fd9a310fca44fccb5c3a9f12b
AtallahFatma <fatma.atallah@gmail.com>
2021-11-24 15:29:05 +0100

    manage modal

--- app/components/GoalInput.js
+++ app/components/GoalInput.js
@@ -1,37 +1,62 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
 import colors from '../config/colors';
 
-function GoalInput({ addGoalHandler }) {
+function GoalInput({ addGoalHandler, modalVisible, setModalVisible }) {
     const [enteredGoal, setEnteredGoal] = useState('');
 
     const goalInputHandler = (text) => {
         setEnteredGoal(text);
     };
 
+    const onAddGoal = () => {
+        addGoalHandler(enteredGoal);
+        setEnteredGoal('');
+    }
+
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder="Course Goal"
-                style={styles.input}
-                onChangeText={goalInputHandler}
-                value={enteredGoal} />
-            <Button title="ADD" onPress={() => addGoalHandler(enteredGoal)} />
-        </View>
+        <Modal animationType="slide"
+            transparent={false}
+            visible={modalVisible}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Course Goal"
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal} />
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.button}>
+                        <Button title="CANCEL" color={"red"} onPress={setModalVisible.bind(this, false)} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="ADD" onPress={onAddGoal} disabled={!enteredGoal} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     input: {
         width: '80%',
         borderColor: colors.black,
         borderWidth: 1,
-        padding: 10
+        padding: 10,
+        margin: 10
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '60%'
+    },
+    button: {
+        width: '40%'
     }
 })
 
--- app/screens/ViewInputScreen.js
+++ app/screens/ViewInputScreen.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { Button, FlatList, SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, StatusBar, View } from 'react-native';
 import GoalInput from '../components/GoalInput';
 import GoalItem from '../components/GoalItem';
 
 function ViewInputScreen() {
     const [courseGoals, setCourseGoals] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
 
     const addGoalHandler = enteredGoal => {
         setCourseGoals(courseGoals => [...courseGoals, { id: Math.random().toString(), value: enteredGoal }]);
+        setModalVisible(false);
     }
 
     const deleteGoalHandler = goalId => {
@@ -21,11 +23,11 @@ function ViewInputScreen() {
             <FlatList
                 ListHeaderComponent={
                     <>
-                        <GoalInput addGoalHandler={addGoalHandler} />
-                        <Button title="Add new goal" />
+                        <GoalInput addGoalHandler={addGoalHandler} modalVisible={modalVisible} setModalVisible={setModalVisible} />
+                        <Button title="Add new goal" onPress={() => setModalVisible(true)} />
                     </>}
                 data={courseGoals}
-                ListEmptyComponent={<Text>Add items to list</Text>}
+                // ListEmptyComponent={<Text>Add items to list</Text>}
                 keyExtractor={item => item.id}
                 renderItem={({ item }) =>
                     <GoalItem value={item.value} id={item.id} onDelete={deleteGoalHandler} />}
@@ -38,7 +40,6 @@ function ViewInputScreen() {
 const styles = StyleSheet.create({
     screen: {
         padding: 200,
-        backgroundColor: 'pink',
         marginVertical: StatusBar.currentHeight || 70,
     }
 })
